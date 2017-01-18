64617659ed1d8bac9c038aa7cb05b69936bfaa70
Steve Russo <svrusso1@gmail.com>
2017-01-18 12:11:57 -0500

    Add boilerplate code from Microsoft example https://support.microsoft.com/en-us/kb/977996

---
+++ App_Domain_Issue_1/Com_1.ClassFactory.cs
@@ -0,0 +1,52 @@
+﻿using ComClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com_1
+{
+
+    /// <summary>
+    /// Class factory for the class ComClass1.
+    /// </summary>
+    internal class ComClass1ClassFactory : IClassFactory
+    {
+        public int CreateInstance(IntPtr pUnkOuter, ref Guid riid,
+            out IntPtr ppvObject)
+        {
+            ppvObject = IntPtr.Zero;
+
+            if (pUnkOuter != IntPtr.Zero)
+            {
+                // The pUnkOuter parameter was non-NULL and the object does 
+                // not support aggregation.
+                Marshal.ThrowExceptionForHR(COMNative.CLASS_E_NOAGGREGATION);
+            }
+
+            if (riid == new Guid(ComClass1.ClassId) ||
+                riid == new Guid(COMNative.IID_IDispatch) ||
+                riid == new Guid(COMNative.IID_IUnknown))
+            {
+                // Create the instance of the .NET object
+                ppvObject = Marshal.GetComInterfaceForObject(
+                    new ComClass1(), typeof(IComClass1));
+            }
+            else
+            {
+                // The object that ppvObject points to does not support the 
+                // interface identified by riid.
+                Marshal.ThrowExceptionForHR(COMNative.E_NOINTERFACE);
+            }
+
+            return 0;   // S_OK
+        }
+
+        public int LockServer(bool fLock)
+        {
+            return 0;   // S_OK
+        }
+    }
+}
---
+++ App_Domain_Issue_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Com_1;
+using ComClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    static void Main(string[] args)
+    {
+        // Run the out-of-process COM server
+        OutOfProcServer.Instance.Run(ComClass1.ClassId, typeof(ComClass1ClassFactory));
+    }
+}
+
